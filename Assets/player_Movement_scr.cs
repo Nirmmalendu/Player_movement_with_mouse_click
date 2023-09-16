2f6d72080772788726d59f149e70332f2acaa8ce
Nirmalendu khan <khan.nupur@gmail.com>
2023-09-17 00:19:45 +0600

    Done!

--- Assets/player_Movement_scr.cs
+++ Assets/player_Movement_scr.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class player_Movement_scr : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public GameObject markerPfb;
+    public float distance = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,28 @@ public class player_Movement_scr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray movePos = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(movePos, out var hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+                RaycastHit hit;
+                if (Physics.Raycast(movePos, out hit, distance))
+                {
+                    Destroy(GameObject.FindWithTag("marker"));
+                    GameObject marker = Instantiate(markerPfb, hit.point, Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "marker")
+        {
+            Destroy(collision.gameObject);
+            
+        }
     }
 }
